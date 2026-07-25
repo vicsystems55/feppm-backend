74bb0ebbcc0a6252b310580a401d4ca5ea12ced4
vicsystems <victorasuquob@gmail.com>
2026-07-25 08:12:00 +0100

    task update

--- src/routes/facilityRoutes.js
+++ src/routes/facilityRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 
 import { getFacility, getFacilityTree, listFacilities } from '../controllers/facilityController.js';
-import { authenticate, requirePermission, requireRole } from '../middleware/auth.js';
+import { authenticate, requirePermission } from '../middleware/auth.js';
 
 const router = Router();
 
 router.use(authenticate, requirePermission('facilities.view'));
 router.get('/', listFacilities);
-router.get('/tree', requireRole('SUPER_ADMIN'), getFacilityTree);
+router.get('/tree', getFacilityTree);
 router.get('/:id', getFacility);
 
 export default router;
